e14309004d81a343a9635694940c6e0b4a4a7a88
jinsukimwebdeveloper <jinsukimwebdeveloper@outlook.kr>
2015-03-26 18:05:01 +0900

    Let's create Issue Tables

--- WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -25,8 +25,7 @@ namespace Jira
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css"));
         }
     }
 }
--- WebApplication1/DB/Table/LoginHistory.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE LoginHistory
-(
-Id int IDENTITY(1,1) PRIMARY KEY,
-UserId int,
-Status int,
-CreatedTimeStamp datetime2 NOT NULL CONSTRAINT DF_LoginHistory_CreateDate_GETDATE DEFAULT GETDATE()
-)
